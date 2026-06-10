02df21a24ef117ad99bcacb264f3b694431d653d
wawow <wawow830@gmail.com>
2026-06-10 22:49:59 +1000

    main setup and also fix require

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,34 @@ use stm32g4xx_hal as hal;
 use hal::{
     prelude::*,
     pwr::PwrExt,
-    rcc:Config,
+    rcc,
     stm32,
     usb::{Peripheral, UsbBus},
-    time::ExtU32,
 };
 
 #[entry]
 fn main() -> ! {
+    let peripherals = stm32::Peripherals::take().unwrap();
+
+    let pwr = peripherals.PWR.constrain().freeze();
+    let mut rcc = peripherals.RCC.freeze(rcc::Config::hsi(), pwr);
+
+    rcc.enable_hsi48();
+
+    let gpioa = peripherals.GPIOA.split(&mut rcc);
+
+    let usb_dm = gpioa.pa11.into_alternate(); // D-
+    let usb_dp = gpioa.pa12.into_alternate(); // D+
+
+    let usb = Peripheral {
+        usb: peripherals.USB,
+        pin_dm: usb_dm,
+        pin_dp: usb_dp,
+    }
+
+    let usb_bus = UsbBus::new(usb);
+
+    loop {
+
+    }
 }
