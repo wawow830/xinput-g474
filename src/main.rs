b90b53609248a070b5804554c74f898ed71d1117
wawow <wawow830@gmail.com>
2026-06-10 22:17:44 +1000

    require dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use cortex_m_rt::entry;
+use panic_probe as _;
+
+use stm32g4xx_hal as hal;
+
+use hal::{
+    prelude::*,
+    pwr::PwrExt,
+    rcc:Config,
+    stm32,
+    usb::{Peripheral, UsbBus},
+    time::ExtU32,
+};
+
+#[entry]
+fn main() -> ! {
 }
